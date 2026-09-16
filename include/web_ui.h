f2ec7af10fb070ae5882df702303d273a125989f
set-st <set-st@users.noreply.github.com>
2026-09-16 13:54:36 +0300

    update

--- include/web_ui.h
+++ include/web_ui.h
@@ -391,7 +391,7 @@ const char PAGE_INDEX[] PROGMEM = R"rawliteral(<!DOCTYPE html>
       
       <div class="toggle-group">
         <div id="btnModeAbs" class="toggle-btn active" onclick="setRelativeMode(false)">Абсолютний кут</div>
-        <div id="btnModeRel" class="toggle-btn" onclick="setRelativeMode(true)">Відносний крок (Δ)</div>
+        <div id="btnModeRel" class="toggle-btn" onclick="setRelativeMode(true)">Відносний кутΔ)</div>
       </div>
 
       <div class="form-group">
